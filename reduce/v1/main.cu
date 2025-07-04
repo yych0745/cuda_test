99993814443873c8f177857acc5142151e4b550f
yych0745 <1398089567@qq.com>
2025-07-04 11:21:29 +0800

    reduce_v1

---
+++ reduce/v1/main.cu
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <cuda_runtime.h>
+
+using PER_THREAD_BLOCK = 256;
+
+__global__ void reduce(int *input, int *output, int n) {
+    int tid = threadIdx.x;
+    int bid = blockIdx.x;
+    input = input + bid * blockDim.x;
+    for (int i = 1; i < PER_THREAD_BLOCK / 2; i *= 2) {
+        if (tid % (2 * i) == 0) {
+            input[tid] += input[tid + i];
+        }
+        __syncthreads();
+    }
+    if (tid == 0) {
+        output[bid] = input[0];
+    }
+}
+
+bool check_equal(float a, float b) {
+    return (a - b) < 1e-6;
+}
+
+
+int main() {
+    const int N=32*1024*1024;
+    float *input = (float *)malloc(n * sizeof(float));
+
+    int block_num = n / PER_THREAD_BLOCK;
+    float *cpu_output = (float *)malloc(block_num * sizeof(float));
+    float *output = (float *)malloc(block_num * sizeof(float));
+
+    for (int i = 0; i < n; i++) {
+        input[i] = i + 0.1;
+    }
+    for (int i = 0; i < block_num; i++) {
+        cpu_output[i] = 0;
+        for (int j = 0; j < PER_THREAD_BLOCK; j++) {
+            cpu_output[i] += input[i * PER_THREAD_BLOCK + j];
+        }
+    }
+
+    int *d_input, *d_output;
+    cudaMalloc((void **)&d_input, n * sizeof(int));
+    cudaMalloc((void **)&d_output, n * sizeof(int));
+
+    cudaMemcpy(d_input, input, n * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_output, output, n * sizeof(int), cudaMemcpyHostToDevice);
+    dim3 grid(block_num);
+    dim3 block(PER_THREAD_BLOCK);
+
+    reduce<<<grid, block>>>(d_input, d_output, n);
+    cudaMemcpy(output, d_output, n * sizeof(int), cudaMemcpyDeviceToHost);
+
+    bool is_equal = true;
+    for (int i = 0; i < block_num; i++) {
+        if (!check_equal(cpu_output[i], output[i])) {
+            std::cout << "Error at block " << i << " " << cpu_output[i] << " " << output[i] << std::endl;
+            is_equal = false;
+            break;
+        }
+    }
+    if (is_equal) {
+        std::cout << "Test passed" << std::endl;
+    } else {
+        std::cout << "Test failed" << std::endl;
+    }
+    cudaFree(d_input);
+    cudaFree(d_output);
+    free(input);
+    free(output);
+    free(cpu_output);
+    return 0;
+}
